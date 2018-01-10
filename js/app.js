2437445a2d2a32a50fd8332fcf6f4c5e390bb501
Carlos <clockwerkz@gmail.com>
2018-01-09 22:52:30 -0800

    Roughing in some code, familiarizing myself with forms

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function formInfo() {
+    let dailyDate = document.getElementById("DailyDate").value;
+    let dailyFourUp = document.getElementById("DailyFourUp").value;
+    let dailyFourDown = document.getElementById("DailyFourDown").value;
+    alert(dailyDate);
+}
